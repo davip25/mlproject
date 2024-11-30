441f18f8541cc47add735213b49fa0f1e26175c3
Eric David <edavid.acosta@gmail.com>
2024-11-30 23:12:14 +0100

    Model trainer and .gitattributes

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -2,6 +2,7 @@ import os
 import sys
 from src.exception import CustomExeption
 from src.logger import logging
+from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
@@ -39,7 +40,10 @@ if __name__ == "__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
 
+    model_trainer = ModelTrainer()
+
+print(model_trainer.initiate_model_trainer(train_arr, test_arr))
 
 
